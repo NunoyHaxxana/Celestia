adcc4217f725824e058e553228c937f45488d0d1
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-07-21 23:45:10 +0700

    Create Bridge_Node.sh

---
+++ Bridge_Node.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo "==========================================================================================================================="
+echo -e "\033[0;35m"
+echo "  ██████╗ ██████╗ ███╗   ██╗████████╗██████╗ ██╗██████╗ ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗██████╗  █████╗  ██████╗ ";
+echo " ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║██╔══██╗██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗";
+echo " ██║     ██║   ██║██╔██╗ ██║   ██║   ██████╔╝██║██████╔╝██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║██║  ██║███████║██║   ██║";
+echo " ██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██╗██║██╔══██╗██║   ██║   ██║   ██║██║   ██║██║╚██╗██║██║  ██║██╔══██║██║   ██║";
+echo " ╚██████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║██████╔╝╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║██████╔╝██║  ██║╚██████╔╝";
+echo "  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ";
+																													  
+echo -e "\033[0;35m"
+echo "==========================================================================================================================="                                                                                    
+sleep 1
+
+source $HOME/.bash_profile
+
+#function Run Celestia Bridge Node
+echo -e "\e[1m\e[32mSetting Up A Celestia Bridge Node \e[0m" && sleep 1
+cd $HOME
+rm -rf celestia-node
+git clone https://github.com/celestiaorg/celestia-node.git
+cd celestia-node/
+git checkout tags/v0.3.0-rc2
+make install
+
+node_ip=$(curl -s ifconfig.me)
+celestia bridge init --core.remote tcp://${node_ip}:26657 --core.grpc tcp://${node_ip}:9090
+celestia bridge init --core.remote tcp://localhost:26657 --core.grpc tcp://localhost:9090
+
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-bridge.service
+[Unit]
+Description=celestia-bridge Cosmos daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/go/bin/celestia bridge start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable celestia-bridge
+sudo systemctl restart celestia-bridge
